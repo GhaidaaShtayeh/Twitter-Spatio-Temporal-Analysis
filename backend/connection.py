5c07370578135f6403e419debf3a05930f623249
ghaidaashtayeh <ghaidaashtayeh@hotmail.com>
2022-12-09 14:21:49 +0200

    quey service added

--- backend/connection.py
+++ backend/connection.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI , Request
 from http import HTTPStatus
 from dataclasses import dataclass
-from elasticsearch import Elasticsearch
-from datetime import datetime
 import uvicorn
-from pydantic import BaseModel
 from fastapi_elasticsearch import ElasticsearchAPIQueryBuilder
-from model.tweet import SerchQuery
-from service.app_service import genarete_query
+from model.app_service import genarete_query
+from model.app_service import connect_elasticsearch
+from model.app_service import SerchQuery
+
+
 
 query_builder = ElasticsearchAPIQueryBuilder()
-from model.tweet import Tweet
 
-    
 app = FastAPI(
     title="IR API",
     description="..",
@@ -27,76 +25,27 @@ def _heath_check() -> dict:
     "status-code": HTTPStatus.OK,
     }
     return response
-
-def connect_elasticsearch():
-    _es = None
-    _es = Elasticsearch("http://localhost:9200/")
-    if _es.ping():
-        print('Yay Connect')
-    else:
-        print('Awww it could not connect!')
-    return _es
-
-def create_index(es_object, index_name):
-    created = False
-    # index settings
-    settings = {
-        "mappings": {
-            "properties": {
-                "created_at": {
-                    "type": "date",
-                    "fields": {
-                        "keyword": {
-                            "type": "keyword",
-                        }
-                    }
-                },
-                "id": {
-                    "type": "keyword",
-                },
-                "id_str": {
-                    "type": "keyword",
-                },
-                "text": {
-                    "type": "text"
-                },
-                "coordinates": {
-                    "type": "geo_point"
-                }
-            }
-
-        }
-    }
-    try:
-        if not es_object.indices.exists(index=index_name):
-            es_object.indices.create(index=index_name, ignore=400,body=settings,timeout=30)
-            created = True
-            print('Created Index')
-    except Exception as ex:
-        print(str(ex))
-    finally:
-        return created
   
 @app.post("/search") 
-async def _search(es , query:SerchQuery) -> dict:
-    print(query)
-    es_query = genarete_query(query)
-    print(es_query)
-    res = es.search(index="tweets", body=es_query,size=100)
-    #filter the res
-    res = res["hits"]["hits"]
-    keywords = ["id","text","created_at","coordinates"]
-    res = [{key:tweet["_source"][key] for key in keywords} for tweet in res]
-    print(len(res))
-    print(es_query)
-    print(query)
-    response = {
-    "message": HTTPStatus.OK.phrase,
-    "status-code": HTTPStatus.OK,
-    "data": res
-    }
-    return response
+async def _search(query:SerchQuery) -> dict:
+    try:
+        es_query = genarete_query(query)
+        print(es_query)
+        es = connect_elasticsearch()
+        resp = es.search(index="some_index", body=es_query,size=100)
+        print("Got %d Hits:" % resp['hits']['total']['value'])
+        for hit in resp['hits']['hits']:
+            print("%(created_at)s id : %(id)s:  text : %(text)s" % hit["_source"])
+        print(len(resp))
+        response = {
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+        "data": resp
+        }
+        return response
+    except Exception as ex:
+        print(str(ex))
   
+
 if __name__ == "__main__":
-    create_index(connect_elasticsearch(),"testing")
     uvicorn.run("connection:app", host="0.0.0.0", port=5000, reload=True)
---
+++ backend/model/app_service.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+from http import HTTPStatus
+from fastapi import FastAPI , Request
+from http import HTTPStatus
+from elasticsearch import Elasticsearch
+
+
+
+
+#from tweet import SerchQuery
+@dataclass
+class GeoPoint:
+    latitude:float = None
+    longitude:float = None
+    radius: float =None
+
+@dataclass
+class SerchQuery:
+    text: str = None
+    start_date: str = None
+    end_date: str = None
+    coordinates: GeoPoint = None
+    
+def genarete_query(query:SerchQuery) -> dict:
+    base_query = {"query": {"bool": {"filter": [] ,"must": [] }}}
+    if query.start_date and query.end_date:
+        base_query["query"]["bool"]["filter"].append(
+            {
+                "range": {
+                    "created_at": {
+                        "gte": query.start_date,
+                        "lte": query.end_date,
+                    }
+                }
+            }
+        )
+        if query.coordinates:
+            base_query["query"]["bool"]["filter"].append(
+            {
+                "geo_distance": {
+                    "distance": f"{query.coordinates.radius}km",
+                    "coordinates": {
+                        "lat": query.coordinates.latitude,
+                        "lon": query.coordinates.longitude,
+                        
+                    }
+                }
+            }
+        )
+    if query.text:
+        base_query["query"]["bool"]["must"].append({"match": {"text": query.text}})
+    return base_query
+
+
+def connect_elasticsearch():
+    _es = None
+    _es = Elasticsearch("http://localhost:9200/")
+    if _es.ping():
+        print('Yay Connect')
+    else:
+        print('Awww it could not connect!')
+    return _es
+
+
+def create_index(es_object, index_name):
+    created = False
+    # index settings
+    settings = {
+        "mappings": {
+            "properties": {
+                "created_at": {
+                    "type": "date",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                        }
+                    }
+                },
+                "id": {
+                    "type": "keyword",
+                },
+                "id_str": {
+                    "type": "keyword",
+                },
+                "text": {
+                    "type": "text"
+                },
+                "coordinates": {
+                    "type": "geo_point"
+                }
+            }
+
+        }
+    }
+    try:
+        if not es_object.indices.exists(index=index_name):
+            es_object.indices.create(index=index_name, ignore=400,body=settings,timeout=30)
+            created = True
+            print('Created Index')
+    except Exception as ex:
+        print(str(ex))
+    finally:
+        return created
