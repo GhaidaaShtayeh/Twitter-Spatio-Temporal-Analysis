e0afe946bac54c26dae15f14f7b0723a60643a12
ghaidaashtayeh <ghaidaashtayeh@hotmail.com>
2022-12-11 19:26:32 +0200

    refactoring

--- backend/connection.py
+++ backend/connection.py
@@ -29,7 +29,7 @@ async def _query(query:query):
     try:
         es = connect_elasticsearch()
         es_query = search_with_filters(query)
-        resp = es.search(index="tweets", body=es_query,size=100)
+        resp = es.search(index="tweets",body={"query": {"match_all": {}}})
         print("Got %d Hits:" % resp['hits']['total']['value'])
         for hit in resp['hits']['hits']:
             print("%(created_at)s id : %(id)s:  text : %(text)s" % hit["_source"])
@@ -50,17 +50,22 @@ async def _query(query:query):
     
 @app.post('/stream_data')
 async def stream_data(query:query):
-    es = connect_elasticsearch()
+    try:    
+        #prepare elastic search context
+        es = connect_elasticsearch()
+        #genarate a query that are applied a filters in the data
+        es_query = search_with_filters(query)
+        # Query Elasticsearch for data
+        data = es.search(index="tweets",body=es_query)
+        for hit in data['hits']['hits']:
+                print("%(created_at)s id : %(id)s:  text : %(text)s" % hit["_source"])
+        # Stream the data out in chunks of 5 seconds
+        for chunk in data:
+            await asyncio.sleep(5)
+            return chunk
+        
+    except Exception as ex:
+        print(str(ex))
     
-    es_query = search_with_filters(query)
-    # Query Elasticsearch for data
-    data = es.search(index="tweets",body=es_query)
-    for hit in data['hits']['hits']:
-            print("%(created_at)s id : %(id)s:  text : %(text)s" % hit["_source"])
-    # Stream the data out in chunks of 5 seconds
-    for chunk in data:
-        await asyncio.sleep(5)
-        return chunk
-
 if __name__ == "__main__":
     uvicorn.run("connection:app", host="0.0.0.0", port=5000, reload=True)
