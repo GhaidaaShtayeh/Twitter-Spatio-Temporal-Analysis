667b729bc09cee00331424aa4bb6a03da22cf266
ghaidaashtayeh <ghaidaashtayeh@hotmail.com>
2022-12-14 21:43:24 +0200

    genarate form and daynamic data in heat map

--- backend/model/app_service.py
+++ backend/model/app_service.py
@@ -4,33 +4,43 @@ from fastapi import FastAPI , Request
 from http import HTTPStatus
 from elasticsearch import Elasticsearch
 
-
-
-
-#from tweet import SerchQuery
+# Use the dataclass decorator to define two classes: GeoPoint and query
 @dataclass
 class GeoPoint:
+    # Define two fields for the GeoPoint class: latitude and longitude, both of which are floating-point numbers
     latitude:float = None
     longitude:float = None
 @dataclass
 class query:
+    # Define four fields for the query class: keywords, start_date, end_date, and coordinates
+    # keywords is a string, while start_date and end_date are also strings
+    # coordinates is an instance of the GeoPoint class
     keywords: str = None
     start_date: str = None
     end_date: str = None
     coordinates: GeoPoint = None
-    
+   
+   
+   # Define a function called connect_elasticsearch() that initializes an Elasticsearch client and attempts to connect to it 
 def connect_elasticsearch():
+    # Initialize the Elasticsearch client to None
     _es = None
+    # Attempt to connect to Elasticsearch on the local machine (http://localhost:9200/)
     _es = Elasticsearch("http://localhost:9200/")
+    # If the connection is successful, print a message
     if _es.ping():
         print('Yay Connect')
+    # If the connection fails, print a different message
     else:
         print('Awww it could not connect!')
+    # Return the Elasticsearch client object
     return _es
 
 def create_index(es_object, index_name): 
+    # Flag to indicate whether the index was created
     created = False
-    # index settings
+    
+    # Dictionary containing index settings
     settings = {
         "mappings": {
             "properties": {
@@ -55,18 +65,21 @@ def create_index(es_object, index_name):
                     "type": "geo_point"
                 }
             }
-
         }
     }
+    
     try:
+        # Check if the index already exists
         if not es_object.indices.exists(index=index_name):
-            es_object.indices.create(index=index_name, ignore=400,body=settings,timeout=30)
+            # If the index doesn't exist, create it
+            es_object.indices.create(index=index_name, ignore=400, body=settings, timeout=30)
             created = True
             print('Created Index')
     except Exception as ex:
         print(str(ex))
     finally:
         return created
+
     
 def search_with_filters(obj: query):
     
